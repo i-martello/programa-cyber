965b56cb32b19588b69f87a9205355453cca41e2
i-martello <martelloignacio@gmail.com>
2023-04-13 07:56:48 -0300

    cambios

--- src/pages/api/gethistorial.ts
+++ src/pages/api/gethistorial.ts
@@ -1,5 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import {historial} from '../../backend/models/pcdata'
+import mongoose from 'mongoose'
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("Base de datos funcionando");
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 export default async function getHistorial(req: NextApiRequest, res: NextApiResponse){
   const data = await historial.find().sort({createdAt: -1});  
